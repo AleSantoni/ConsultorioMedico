c7d2caef38dfb7595de9bb4daf66cbaeb9538b72
Alejandro <asantoni888@hotmail.com>
2024-03-06 20:30:50 -0300

    App Pacientes lista

--- consultorio/paciente/models.py
+++ consultorio/paciente/models.py
@@ -1,16 +1,22 @@
 from django.db import models
+from obrasocial.models import ObraSocial
 
 # Create your models here.
 
 class Paciente(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
-    dni = models.IntegerField()
+    edad = models.IntegerField(default=0, blank=False, null=False)
+    dni = models.CharField(max_length=8)
     fecha_nacimiento = models.DateField( auto_now=False, auto_now_add=False)
     telefono = models.IntegerField( )
     email = models.EmailField()
     direccion = models.CharField(max_length=100)
-    observaciones = models.TextField(max_length=200)
+    observaciones = models.TextField()
+    obrasocial=models.ForeignKey(ObraSocial, on_delete=models.CASCADE)
+   
+    
+    
 
     def __str__(self):
         return self.nombre
